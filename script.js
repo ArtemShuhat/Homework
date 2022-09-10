e711c230a309c39a179dd3f4563519ff052a53e5
ArtemShuhat <shuhat.artem2006@gmail.com>
2022-09-10 17:10:35 +0300

    Sweets

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
 let count = prompt('Сколько конфет?');//Изначальное количество конфет
-let n = count/2;
 while(count > 0){
     let sweet = confirm('Еще конфету?');//Текущее количество конфет
     if (sweet == true){
